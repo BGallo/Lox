c0ee3037087c85662c97e121c7f1075ecded1579
BGallo <bgallo@id.uff.br>
2022-08-21 09:39:32 -0300

    OK

--- Java/tool/GenerateAst.java
+++ Java/tool/GenerateAst.java
@@ -1,4 +1,4 @@
-package tool;
+//package tool;
 
 
 import java.io.IOException;
@@ -27,20 +27,37 @@ public class GenerateAst {
         String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package lox;");
+        writer.println("package Lox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+         // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
+    }
+    private static void defineVisitor(
+      PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" +
+          typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
     }
     private static void defineType(
             PrintWriter writer, String baseName,
@@ -59,6 +76,13 @@ public class GenerateAst {
         }
 
         writer.println("    }");
+         writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+        className + baseName + "(this);");
+        writer.println("    }");
+
 
         // Fields.
         writer.println();
